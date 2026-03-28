cb8aaa4f0c8a774db6d23d7e0763c88bde425aa8
Nandan Nalwade <185660510+Nandan2006-oss@users.noreply.github.com>
2026-03-28 21:53:45 +0530

    added 2 arrays to store the id array of the image and also their labels

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -2,16 +2,41 @@ import os
 import numpy as np
 from PIL import Image
 
-folder = "test/fake"
+fake_folder = "test/fake"
+real_folder = "test/real"
 
-for file in os.listdir(folder)[1:5]:
-    path = os.path.join(folder, file)
+X = []
+y = []
+
+for file in os.listdir(fake_folder)[1:5]:
+    path = os.path.join(fake_folder, file)
 
     image = Image.open(path)
     image = image.resize((128, 128))
     image = image.convert("RGB")
     
     image_array = np.array(image)
+    X.append(image_array.flatten())
+    y.append(1)
 
     print(image.size)
     print(image.mode)
+
+for file in os.listdir(real_folder)[1:5]:
+    path = os.path.join(real_folder, file)
+
+    image = Image.open(path)
+    image = image.resize((128, 128))
+    image = image.convert("RGB")
+    
+    image_array = np.array(image)
+    X.append(image_array.flatten())
+    y.append(0)
+
+    print(image.size)
+    print(image.mode)
+
+print(len(X), len(y))
+print(np.array(X).shape)
+print(X)
+print(y)
